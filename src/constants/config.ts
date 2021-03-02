9a61832a605b61e461520328796f717d4a9015db
Rose Bilag <hello@rosebilag.com>
2021-03-02 14:26:10 +0800

    updated player config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,9 +2,7 @@ export const PLAYER_CONFIG = {
 	youtube: {
 		playerVars: {
 			disablekb: 1,
-			iv_load_policy: 3,
-			origin: 'http://localhost:3000',
-			widget_referrer: 'http://localhost:3000'
+			iv_load_policy: 3
 		}
 	}
 };
